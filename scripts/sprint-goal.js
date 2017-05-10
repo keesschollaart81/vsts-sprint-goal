c53b33c4e4a23eeb4ccdccaac01b65819feed3d4
Kees Schollaart <keesschollaart81@hotmail.com>
2017-05-10 06:42:34 +0200

    First internal demo version

--- scripts/sprint-goal.js
+++ scripts/sprint-goal.js
@@ -1,10 +1,62 @@
-define(["require", "exports", "q"], function (require, exports, Q) {
+define(["require", "exports", "q", "VSS/Controls", "VSS/Controls/Menus", "VSS/Controls/StatusIndicator"], function (require, exports, Q, Controls, Menus, StatusIndicator) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     var SprintGoal = (function () {
         function SprintGoal() {
             var _this = this;
+            this.getTabTitle = function (tabContext) {
+                console.log('getTabTitle');
+                if (tabContext && tabContext.iterationId) {
+                    _this.iterationId = tabContext.iterationId;
+                    var goalText = _this.getCookie(_this.iterationId + "goalText");
+                    var sprintGoalInTabLabel = _this.getCookie(_this.iterationId + "sprintGoalInTabLabel");
+                    if (sprintGoalInTabLabel == "true" && goalText != null)
+                        return "Goal: " + goalText.substr(0, 60) + "";
+                    else
+                        return "Goal";
+                }
+                else {
+                    tabContext;
+                    return "Goal";
+                }
+            };
+            this.saveSettings = function () {
+                console.log('saveSettings');
+                _this.waitControl.startWait();
+                var sprintConfig = {
+                    sprintGoalInTabLabel: $("#sprintGoalInTabLabel").prop("checked"),
+                    goal: $("#goal").val()
+                };
+                _this.setCookie(_this.iterationId + "goalText", sprintConfig.goal);
+                _this.setCookie(_this.iterationId + "sprintGoalInTabLabel", sprintConfig.sprintGoalInTabLabel);
+                VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
+                    dataService.setValue("sprintConfig." + _this.iterationId, sprintConfig).then(function (value) {
+                        _this.waitControl.endWait();
+                    });
+                });
+            };
+            this.getSettings = function () {
+                console.log('getSettings');
+                _this.waitControl.startWait();
+                VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
+                    Q.when(dataService.getValue("sprintConfig." + _this.iterationId), function (object) {
+                        if (object) {
+                            $("#sprintGoalInTabLabel").prop("checked", object.sprintGoalInTabLabel);
+                            $("#goal").val(object.goal);
+                            _this.setCookie(_this.iterationId + "goalText", object.goal);
+                            _this.setCookie(_this.iterationId + "sprintGoalInTabLabel", object.sprintGoalInTabLabel);
+                        }
+                        _this.waitControl.endWait();
+                    });
+                });
+            };
             console.log('constructor');
+            var waitControlOptions = {
+                target: $("#sprint-goal"),
+                cancellable: false,
+                backgroundColor: "#ffffff"
+            };
+            this.waitControl = Controls.create(StatusIndicator.WaitControl, $("#sprint-goal"), waitControlOptions);
             if (VSS.getContribution().type === "ms.vss-web.tab") {
                 VSS.register(VSS.getContribution().id, {
                     pageTitle: this.getTabTitle,
@@ -15,39 +67,36 @@ define(["require", "exports", "q"], function (require, exports, Q) {
                 });
             }
             $('.saveButton').on('click', function (eventObject) {
-                _this.saveSettings(_this.iterationId);
+                _this.saveSettings();
             });
-            this.getSettings(this.iterationId);
-        }
-        SprintGoal.prototype.getTabTitle = function (tabContext) {
-            console.log('getTabTitle');
-            if (tabContext && tabContext.iterationId) {
-                return "Goal " + tabContext.iterationId.substr(0, 5) + "";
-            }
-            else {
-                tabContext;
-                return "Goal";
-            }
-        };
-        SprintGoal.prototype.saveSettings = function (iterationId) {
-            console.log('saveSettings');
-            var sprintConfig = {
-                sprintGoalInTabLabel: $("#sprintGoalInTabLabel").prop("checked"),
-                goal: $("#goal").val()
+            this.getSettings();
+            var menuItems = [
+                { id: "save", text: "Save", icon: "icon-save" }
+            ];
+            var menubarOptions = {
+                items: menuItems,
+                executeAction: function (args) {
+                    var command = args.get_commandName();
+                    switch (command) {
+                        case "save":
+                            _this.saveSettings();
+                            break;
+                        default:
+                            alert("Unhandled action: " + command);
+                            break;
+                    }
+                }
             };
-            VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
-                dataService.setValue("sprintConfig." + iterationId, sprintConfig).then(function (value) {
-                });
-            });
+            var menubar = Controls.create(Menus.MenuBar, $(".toolbar"), menubarOptions);
+        }
+        SprintGoal.prototype.setCookie = function (key, value) {
+            var expires = new Date();
+            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
         };
-        SprintGoal.prototype.getSettings = function (iterationId) {
-            console.log('getSettings');
-            VSS.getService(VSS.ServiceIds.ExtensionData).then(function (dataService) {
-                Q.when(dataService.getValue("sprintConfig." + iterationId), function (object) {
-                    $("#sprintGoalInTabLabel").prop("checked", object.sprintGoalInTabLabel);
-                    $("#goal").val(object.goal);
-                });
-            });
+        SprintGoal.prototype.getCookie = function (key) {
+            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
         };
         return SprintGoal;
     }());
--- src/scripts/sprint-goal.ts
+++ src/scripts/sprint-goal.ts
@@ -3,12 +3,22 @@ import Q = require("q");
 import VSS_Service = require("VSS/Service");
 import TFS_Build_Contracts = require("TFS/Build/Contracts");
 import TFS_Build_Extension_Contracts = require("TFS/Build/ExtensionContracts");
+import Controls = require("VSS/Controls");
+import Menus = require("VSS/Controls/Menus");
+import StatusIndicator = require("VSS/Controls/StatusIndicator");
 
 export class SprintGoal {
     private iterationId: number;
+    private waitControl: StatusIndicator.WaitControl;
 
     constructor() {
         console.log('constructor');
+        var waitControlOptions: StatusIndicator.IWaitControlOptions = {
+            target: $("#sprint-goal"),
+            cancellable: false,
+            backgroundColor: "#ffffff"
+        };
+        this.waitControl = Controls.create(StatusIndicator.WaitControl, $("#sprint-goal"), waitControlOptions);
         if (VSS.getContribution().type === "ms.vss-web.tab") {
             VSS.register(VSS.getContribution().id, {
                 pageTitle: this.getTabTitle,
@@ -20,40 +30,87 @@ export class SprintGoal {
         }
 
         $('.saveButton').on('click', (eventObject) => {
-            this.saveSettings(this.iterationId);
+            this.saveSettings();
         });
 
-        this.getSettings(this.iterationId);
+        this.getSettings();
 
+        var menuItems: Menus.IMenuItemSpec[] = [
+            { id: "save", text: "Save", icon: "icon-save" }
+        ];
+        var menubarOptions: Menus.MenuOwnerOptions = {
+            items: menuItems,
+            executeAction: (args) => {
+                var command = args.get_commandName();
+                switch (command) {
+                    case "save":
+                        this.saveSettings();
+                        break;
+                    default:
+                        alert("Unhandled action: " + command);
+                        break;
+                }
+            }
+        };
+
+        var menubar = Controls.create(Menus.MenuBar, $(".toolbar"), menubarOptions);
     }
-    public getTabTitle(tabContext) {
+
+    public getTabTitle = (tabContext) => {
         console.log('getTabTitle');
         if (tabContext && tabContext.iterationId) {
-            return "Goal " + tabContext.iterationId.substr(0, 5) + "";
+            this.iterationId = tabContext.iterationId;
+
+            var goalText = this.getCookie(this.iterationId + "goalText");
+            var sprintGoalInTabLabel = this.getCookie(this.iterationId + "sprintGoalInTabLabel");
+            if (sprintGoalInTabLabel == "true" && goalText != null) return "Goal: " + goalText.substr(0, 60) + "";
+            else return "Goal";
         } else {
             tabContext
             return "Goal";
         }
     }
-    public saveSettings(iterationId: number) {
+
+    public saveSettings = () => {
         console.log('saveSettings');
+        this.waitControl.startWait();
         const sprintConfig = {
             sprintGoalInTabLabel: $("#sprintGoalInTabLabel").prop("checked"),
             goal: $("#goal").val()
         };
+        this.setCookie(this.iterationId + "goalText", sprintConfig.goal);
+        this.setCookie(this.iterationId + "sprintGoalInTabLabel", sprintConfig.sprintGoalInTabLabel);
         VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService: Extension_Data.ExtensionDataService) => {
-            dataService.setValue("sprintConfig." + iterationId, sprintConfig).then((value: object) => {
+            dataService.setValue("sprintConfig." + this.iterationId, sprintConfig).then((value: object) => {
+                this.waitControl.endWait();
             });
         });
     }
-    public getSettings(iterationId: number) {
+    public getSettings = () => {
         console.log('getSettings');
+        this.waitControl.startWait();
         VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService: Extension_Data.ExtensionDataService) => {
-            Q.when(dataService.getValue("sprintConfig." + iterationId), (object: any) => {
-                $("#sprintGoalInTabLabel").prop("checked", object.sprintGoalInTabLabel);
+            Q.when(dataService.getValue("sprintConfig." + this.iterationId), (object: any) => {
+                if (object) {
+                    $("#sprintGoalInTabLabel").prop("checked", object.sprintGoalInTabLabel);
                     $("#goal").val(object.goal)
+                    this.setCookie(this.iterationId + "goalText", object.goal);
+                    this.setCookie(this.iterationId + "sprintGoalInTabLabel", object.sprintGoalInTabLabel);
+                }
+                this.waitControl.endWait();
             });
         });
+    }
+
+    public setCookie(key, value) {
+        var expires = new Date();
+        expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+    }
+
+    public getCookie(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        return keyValue ? keyValue[2] : null;
     }
 }
 
